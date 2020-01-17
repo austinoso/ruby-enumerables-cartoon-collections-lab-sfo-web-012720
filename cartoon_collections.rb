91207cb25a2e0c381c830b5bedb2f888ecc4e6b5
Austin Osorio <osorio_austin@yahoo.com>
2020-01-17 07:35:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,9 +13,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
+  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.include?(cheese_types)
-    true
-  end
-  nil
+  array.include?(cheese_types)
+
 end
