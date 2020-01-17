a58717c54044a84818c08beb69182df647b86598
Austin Osorio <osorio_austin@yahoo.com>
2020-01-17 07:21:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.collect { |x| x.titlecase + "!"}
+  array.collect { |x| x.titlecase + "!" }
 end
 
 def long_planeteer_calls# code an argument here
