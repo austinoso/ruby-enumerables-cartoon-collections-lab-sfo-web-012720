1338fc59bae04cf9e895ab325b3e595209c5c3f9
Austin Osorio <osorio_austin@yahoo.com>
2020-01-17 07:40:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,6 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.index(cheese_types)
+  array.index("cheddar", "gouda", "camembert")
 
 end
